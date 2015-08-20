de715f4440fdcebc898e40e6ade02d27fd34596a
jeroenpeeters <jeroen.peeters@ictu.nl>
2015-08-20 14:12:21 +0200

    docker dhcpd

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM debian:jessie
+
+RUN DEBIAN_FRONTEND=noninteractive && \
+    apt-get update && \
+    apt-get install -y isc-dhcp-server
+
+ADD https://raw.githubusercontent.com/jpetazzo/pipework/master/pipework /pipework
+ADD dhcpd.sh /dhcpd.sh
+ADD dhcpd.conf.template /dhcpd.conf.template
+
+RUN chmod +x /dhcpd.sh /pipework
+
+ENV SUBNET _
+ENV NETMASK _
+ENV RANGE _
+
+EXPOSE 67
+EXPOSE 67/udp
+EXPOSE 547
+EXPOSE 547/udp
+EXPOSE 647
+EXPOSE 647/udp
+EXPOSE 847
+EXPOSE 847/udp
+
+ENTRYPOINT ["/dhcpd.sh"]
+CMD ["-f", "-cf", "/data/dhcpd.conf", "-lf", "/data/dhcpd.leases", "--no-pid"]
---
+++ dhcpd.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir -p /data
+
+echo "waiting on eth1"
+/pipework --wait -i eth1
+
+if [ ! -f /data/dhcpd.conf ]; then
+  if [ "$SUBNET" == '_' ];  then echo "SUBNET is not set"; exit 1; fi
+  if [ "$NETMASK" == '_' ]; then echo "NETMASK is not set"; exit 1; fi
+  if [ "$RANGE" == '_' ];   then echo "RANGE is not set"; exit 1; fi
+
+  cp /dhcpd.conf.template /data/dhcpd.conf
+
+  sed -i "s/_SUBNET_/$SUBNET/g" /data/dhcpd.conf
+  sed -i "s/_NETMASK_/$NETMASK/g" /data/dhcpd.conf
+  sed -i "s/_RANGE_/$RANGE/g" /data/dhcpd.conf
+fi
+
+if [ ! -f /data/dhcpd.leases ]; then
+    touch /data/dhcpd.leases
+fi
+
+exec /usr/sbin/dhcpd $@
